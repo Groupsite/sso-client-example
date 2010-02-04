68b9c9b259002963bb181f7481ab7a2b82a43ad9
Paul Barry <mail@paulbarry.com>
2010-02-04 17:39:45 -0500

    Added method to format a user as a member record for the Sync API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ActiveRecord::Base
+  after_create :add_sync
+  after_update :update_sync
+  
+  def add_sync
+    
+  end
+  
+  def update_sync
+    
+  end
+  
+  def to_record_xml
+    builder = Builder::XmlMarkup.new :indent => 2
+    builder.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+    builder.record(:type => "Member", :xuid => id) do |xml|
+      xml.attribute(email, :name => "primary_email")
+      xml.attribute(first_name, :name => "first_name")
+      xml.attribute(last_name, :name => "last_name")
+      xml.attribute(gender, :name => "gender")
+      xml.attribute("Easter Time (US & Canada)", :name => "timezone")
+      xml.attribute(city, :name => "city")
+      xml.attribute(state, :name => "state")
+      xml.attribute("United States", :name => "country")
+      xml.attribute(zip, :name => "postal_code")
+    end
+  end
+  
 end
